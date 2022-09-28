8380a15ed8c94006f1d1fa9717c7ce6963665c84
HARRIS-IRFAN-123 <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2022-09-28 05:41:25 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+  import React from 'react'
 import './App.css';
-
+import COUNTER from './components/COUNTER.JS';
+  function Welcome (props){
+    return <h1>HEY!!!, {props.name}</h1>
+  }
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Welcome name = "Muazz"/>
+    <Welcome name = "Shahbaz"/>
+    <Welcome name = "Hamza"/>
+    <COUNTER></COUNTER>
+   </div>
   );
 }
 
